56cd5cba4022481b3c8d90b8e6a7909ec5a9b745
Kyle Moore <moore.kyle39@gmail.com>
2019-10-25 02:54:42 -0400

    comments and more specific structs

--- src/simulation/DBClasses.go
+++ src/simulation/DBClasses.go
@@ -3,38 +3,59 @@ package main
 import (
 )
 
+// candles with cointained class, indicators
 
 type Candle struct {
-	Open float32,
-	Close float32,
-	High float32,
-	Low float32,
-	Volume int,
-	Symbol string,
-	Indicators map[string]float32
+	Open float32
+	Close float32
+	High float32
+	Low float32
+	Volume int
+	Symbol string
+	Indicators Indicators
+}
+
+type Indicators struct {
+  // rs is AvgUp/AvgDown
+	AvgUp float32
+	AvgDown float32
+	MAs map[string]float32
+}
+
+func (c *Candle) rsi() float32 {
+    return 100.0 - 100.0 / (1 + c.Indicators.AvgUp / c.Indicators.AvgDown)
+}
+
+// price levels measured with tests
+
+type PriceLevel struct {
+	Price float32
+	// probably should point to tests not sure how Mongo/Go handle this
+	Tests [] Test
 }
 
 type Test struct {
-	Length int,
-	Candles [] Candle,
+	Length int
+	// probably should point to candles not sure how Mongo/Go handle this
+	Candles [] Candle
 }
 
-type PriceLevel struct {
-	Price float32,
-	Tests [] Test
-}
+// 
+
 type Symbol struct {
-	Symbol string,
-	Trading bool,
-	Price_levels [] PriceLevel,
+	Symbol string
+	Trading bool
+	Price_levels [] PriceLevel
 }
 
-/* Option encapsulates a stock option, with methods to calculate 
+/* Option encapsulates a stock option, with methods to calculate
     its price */
 type Option struct {
-	Symbol string,
-	Expiry int, 
+	Symbol string
+	Expiry int
 	Strike int
 }
 
+/*
 
+*/
