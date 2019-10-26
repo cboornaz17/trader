ff8de04b084b9db409f0f0b9162bd460a35c86e2
Corey Boornazian <boornc@coreys-mbp.home>
2019-10-26 13:30:20 -0400

    Update db stuff

--- src/simulation/DBClasses.go
+++ src/simulation/DBClasses.go
@@ -1,40 +1,111 @@
 package main
 
 import (
+	"fmt"
+
+	"log"
+	"context"
+
+	bson "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-
 type Candle struct {
-	Open float32,
-	Close float32,
-	High float32,
-	Low float32,
-	Volume int,
-	Symbol string,
-	Indicators map[string]float32
+	Open float32;
+	Close float32;
+	High float32;
+	Low float32;
+	Volume int;
+	Symbol string;
+	Indicators bson.A;
 }
 
+
+
 type Test struct {
-	Length int,
-	Candles [] Candle,
+	Length int;
+	Candles [] Candle;
 }
 
 type PriceLevel struct {
-	Price float32,
-	Tests [] Test
+	Price float32;
+	Tests [] Test;
 }
 type Symbol struct {
-	Symbol string,
-	Trading bool,
-	Price_levels [] PriceLevel,
+	Symbol string;
+	Trading bool;
+	Price_levels [] PriceLevel;
 }
 
 /* Option encapsulates a stock option, with methods to calculate 
     its price */
 type Option struct {
-	Symbol string,
-	Expiry int, 
-	Strike int
+	Symbol string;
+	Expiry int; 
+	Strike int;
 }
 
+
+type MongoWrapper struct {
+	Db *mongo.Database;
+	client *mongo.Client;
+}
+
+func getData(c mongo.Database) {
+	
+}
+
+func (mw MongoWrapper) insertCandle(c Candle) {
+	fmt.Println("Inserting candle: ", c)
+
+	db := mw.Db
+	collection := db.Collection(c.Symbol)
+
+	insertResult, err := collection.InsertOne(context.TODO(), c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+
+}
+
+func (mw MongoWrapper) getCandle(filter bson.D{}) Candle {
+	
+}
+
+
+func Init() MongoWrapper {
+	fmt.Println("Init")
+
+	
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://192.168.99.100:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db := client.Database("trader")
+
+	mw := MongoWrapper{db, client}
+
+	
+	return mw
+}
+
+func (mw MongoWrapper) Cleanup() error {
+	return mw.client.Disconnect(context.TODO())
+}
 
--- src/simulation/backtest.go
+++ src/simulation/backtest.go
@@ -2,11 +2,43 @@ package main
 
 import (
 	"fmt"
-	"DBClasses"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+    _ "go.mongodb.org/mongo-driver/mongo"
+	_"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	fmt.Println("Hello from go")
 
-	fmt.Println()
+	fmt.Println("Using db classes")
+
+	c := Candle {
+		12.2,
+		12.1,
+		5.2,
+		10.1,
+		1000,
+		"Testing",
+		bson.A{},
+	}
+
+	fmt.Println(c)
+
+	mw := Init()
+
+	fmt.Println("Got db: ", mw)
+
+	mw.insertCandle(c)
+
+	err := mw.Cleanup()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connection to MongoDB closed.")
+
+	
+
 }
